d76647bf426566cdbc86611a13b71bfd277d1399
unknown <antoniofalcaoneto@gmail.com>
2014-11-17 22:32:59 -0200

    Adicionando style a página

--- index.php
+++ index.php
@@ -3,11 +3,31 @@
 <head>
 <meta charset="utf-8">
 <title>Curso de Git</title>
+<style>
+	body {
+		background:#CCC;
+	}
+	#container {
+		width:960px;
+		margin:auto;
+		background:#FFF;
+		height:100%;
+	}
+	h1, h2 {
+		margin:10px;
+		font-weight:bold;
+	}
+	p {
+		margin:10px;
+	}
+</style>
 </head>
 
 <body>
-<h1>Aula de Git</h1>
-<h2>O que é o Loren Ipsun</h2>
-<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
+<div id="container">
+    <h1>Aula de Git</h1>
+    <h2>O que é o Loren Ipsun</h2>
+    <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
+</div>    
 </body>
 </html>
